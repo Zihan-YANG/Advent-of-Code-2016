8255559b02c920152bd514919c2fa7c6f7f1164a
Zihan-YANG <1155141697@link.cuhk.edu.hk>
2024-01-30 14:11:56 +0800

    Day 1 - Part 2 Completed

--- day_01.py
+++ day_01.py
@@ -1,12 +1,21 @@
 # Starting condition
 position_x, position_y = 0, 0
 current_direction = 'N'
+visited_position = []
+ebhq_position = False 
 # Direction map dic
 direction_map = {
 'N': {'L':'W','R':'E'},
 'E': {'L':'N','R':'S'},
 'S': {'L':'E','R':'W'},
 'W': {'L':'S','R':'N'}
+}
+# Coordinate map dic
+coordicate_map = {
+    'N': (0, 1),
+    'S': (0, -1),
+    'E': (1, 0),
+    'W': (-1, 0)
 }
 
 # Input
@@ -19,19 +28,19 @@ for instruction in input_text.split(', '):
     step_text = instruction[1:]
     steps = int(step_text)
     new_direction = direction_map[current_direction][left_right]
-    if new_direction == 'N':
-        position_y = position_y + steps 
-    elif new_direction == 'S':
-        position_y = position_y - steps
-    elif new_direction == 'E':
-        position_x = position_x + steps
-    elif new_direction == 'W':
-        position_x = position_x - steps
-    else:
-        print(f'Wrong direction: {new_direction}')
-    # Record the new direction
-    current_direction = new_direction
+    coordinate_adjustment = coordicate_map[new_direction]
 
+    for step in range(steps):
+        position_x = position_x + coordinate_adjustment[0]
+        position_y = position_y + coordinate_adjustment[1]
+        if (position_x, position_y) in visited_position:
+            ebhq_position = True
+            break
+        visited_position.append((position_x, position_y))
+    if ebhq_position:
+        break 
+    current_direction = new_direction
+   
 # Print result
 print(f'Ending position: ({position_x}, {position_y})')
 print(f'Distance: {abs(position_x) + abs(position_y)}')
